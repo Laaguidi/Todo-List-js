862b168781614e9afe1aa7898468294b84b00324
Yassine Ait Laaguidi <aitlaaguidiyassine@gmail.com>
2022-03-21 16:34:00 +0100

    savelocal todo

--- app.js
+++ app.js
@@ -86,3 +86,17 @@ function filterTodo(e) {
     });
   }
 
+// save in local storage:
+function saveLocalTodos(todo) {
+    let todos;
+    if (localStorage.getItem("todos") === null) {
+      todos = [];
+    } else {
+      todos = JSON.parse(localStorage.getItem("todos"));
+    }
+    todos.push(todo);
+    localStorage.setItem("todos", JSON.stringify(todos));
+  }
+  
+  
+ 
